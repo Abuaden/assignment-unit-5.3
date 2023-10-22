0c853cff23c01abcaa34df67a79ff0608fa4ca23
Abubakar Aden <abubakar.aden978@gmail.com>
2023-10-22 17:57:40 -0500

    added Function findbyArtis and completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,7 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
 let myCollection = [];
-function addToCollection(title,artist,yearPublished){
+function addToCollection(collection,title,artist,yearPublished){
   let newAlbum= {
     title,
     artist,
@@ -19,35 +19,38 @@ addToCollection(myCollection,"2014 Forest Hills Drive", "J.Cole",2014);
 addToCollection(myCollection,"Candy Rain", "Soul for Real",1995);
 addToCollection(myCollection,"What's Going On", "Marvin Gaye",1971);
 addToCollection(myCollection,"Abbey Road", "The Beatles",1969);
+
 console.log(myCollection);
 
-function showCollection(collection){
-  for(let i = 0; i<collection.length; i++){
-     let album = collection[i];
-    console.log(album.title + album.artist + album.yearPublished);
+
+function showCollection(collection) {
+  for (let i = 0; i < collection.length; i++) {
+    let album = collection[i];
+    console.log(album.title + " by " + album.artist + ", published in " + album.yearPublished);
   }
-}
-function findByArtist(collection, artist){
-  let mat
-  for(let i =0; i<collection.length; i++){
+} 
+showCollection(myCollection);
 
+function findByArtist (collection,artist) {
+  let results = [];
+  for(let album of collection){
+    if(album.artist === artist){
+      results.push(album);
+    }
   }
+  return results
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(findByArtist('Michael Jackson',myCollection));
+console.log(findByArtist('Kendrick Lamar',myCollection));
+console.log(findByArtist('J.Cole',myCollection));
+console.log(findByArtist('Soul for Real',myCollection));
+console.log(findByArtist('Marvin Gaye',myCollection));
+console.log(findByArtist('The Beatles',myCollection));
+console.log(findByArtist('Taylor Swift',myCollection));
+console.log(findByArtist('Drake',myCollection));
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
